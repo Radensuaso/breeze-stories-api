ae0f420aafd6b9e6391d31cc8dfac15a7d3feaee
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2021-10-22 11:07:04 +0100

    Cloudinary set

--- src/lib/cloudinaryTools.ts
+++ src/lib/cloudinaryTools.ts
@@ -1,6 +1,23 @@
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage, Options } from "multer-storage-cloudinary";
 
+process.env.TS_NODE_DEV && require("dotenv").config();
+if (!process.env.CLOUD_NAME) {
+  throw new Error("No Cloudinary name.");
+}
+if (!process.env.CLOUD_KEY) {
+  throw new Error("No Cloudinary key.");
+}
+if (!process.env.CLOUD_SECRET) {
+  throw new Error("No Cloudinary secret.");
+}
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
 //================ Avatars
 
 export const saveAvatarCloudinary = new CloudinaryStorage({
