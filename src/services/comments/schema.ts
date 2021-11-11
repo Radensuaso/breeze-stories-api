6de3df332afa254baa62372645565ada951ec284
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2021-11-11 10:12:14 +0000

    Deleted subcomments

--- src/services/comments/schema.ts
+++ src/services/comments/schema.ts
@@ -1,24 +1,12 @@
 import mongoose from "mongoose";
-import { SubCommentDocument, CommentDocument } from "../../../typings/Comment";
+import { CommentDocument } from "../../../typings/Comment";
 
 const { Schema } = mongoose;
-
-const SubCommentSchema = new Schema<SubCommentDocument>(
-  {
-    author: { type: Schema.Types.ObjectId, required: true, ref: "author" },
-    subComment: { type: String, required: true },
-    hearts: { type: [Schema.Types.ObjectId], required: true, ref: "author" },
-  },
-  { timestamps: true }
-);
-
 const CommentSchema = new Schema<CommentDocument>(
   {
     story: { type: Schema.Types.ObjectId, required: true, ref: "story" },
     author: { type: Schema.Types.ObjectId, required: true, ref: "author" },
     comment: { type: String, required: true },
-    hearts: { type: [Schema.Types.ObjectId], required: true, ref: "author" },
-    subComments: { type: [SubCommentSchema] },
   },
   {
     timestamps: true,
--- typings/Comment.d.ts
+++ typings/Comment.d.ts
@@ -2,24 +2,12 @@ import { Document, Model } from "mongoose";
 import { AuthorDocument } from "./Author";
 import { StoryDocument } from "./Story";
 
-//============= Sub Comment
-
-export interface SubComment {
-  author: AuthorDocument;
-  subComment: string;
-  hearts: AuthorDocument[];
-}
-
-export interface SubCommentDocument extends Document, SubComment {}
-
 //================ Comment
 
 export interface Comment {
   story: StoryDocument;
   author: AuthorDocument;
   comment: string;
-  hearts: AuthorDocument[];
-  subComments: SubCommentDocument[];
 }
 
 export interface CommentDocument extends Document, Comment {}
